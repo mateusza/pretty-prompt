b08c8bcdf06ff77d6d02b24d734db2d93aad5ca9
Mateusz Adamowski <mateusz@adamowski.pl>
2019-03-17 13:37:59 +0000

    centos fix + fedora

--- pretty-prompts.sh
+++ pretty-prompts.sh
@@ -15,7 +15,8 @@ if [[ "$TERM" == "xterm" && "$SHELL" == "/bin/bash" ]] ; then
 	case "$os_family" in
 		ubuntu)		colors=(202 241 230 090 127 255) ;;
 		void)		colors=(036 255 114 255 113 255) ;;
-		centos)		colors=(214 255 112 255 090 255) ;;
+		centos)		colors=(214 018	112 255 090 255) ;;
+		fedora)		colors=(025 244 255 255 027 255) ;;
 		Linux)		colors=(220 243 220 243 220 255) ;;
 		*)		colors=(227 221 215 209 203 197) ;;
 	esac
