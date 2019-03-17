74d86119aade9d5ff0298d5dc35aa9acb9f66b58
Mateusz Adamowski <mateusz@adamowski.pl>
2019-03-17 13:29:06 +0000

    refactor, centos support

--- pretty-prompts.sh
+++ pretty-prompts.sh
@@ -1,42 +1,38 @@
 #!/bin/bash
 
-set_prompt(){
-	local colors
-	colors=( $1 )
-	
-        PS1="\[\e]0;\u@\h: \w\a\]"
+if [[ "$TERM" == "xterm" && "$SHELL" == "/bin/bash" ]] ; then
+
+	if [[ -n "$1" ]]; then
+		os_family="$1"
+	else
+		if [[ -e /etc/os-release ]]; then
+			os_family="$( sed -ne '/^ID=/s/^ID="\?\([^"]*\)"\?$/\1/p' /etc/os-release )"
+		else
+			os_family="$( uname -a )"
+		fi
+	fi
+
+	case "$os_family" in
+		ubuntu)		colors=(202 241 230 090 127 255) ;;
+		void)		colors=(036 255 114 255 113 255) ;;
+		centos)		colors=(214 255 112 255 090 255) ;;
+		Linux)		colors=(220 243 220 243 220 255) ;;
+		*)		colors=(227 221 215 209 203 197) ;;
+	esac
+
+	PS1="\[\e]0;\u@\h: \w\a\]"
 	PS1+="\[\e[0;38;5;${colors[0]}m\]\u"
 	PS1+="\[\e[0;38;5;${colors[1]}m\]@"
 	PS1+="\[\e[0;38;5;${colors[2]}m\]\h"
 	PS1+="\[\e[0;38;5;${colors[3]}m\] "
 	PS1+="\[\e[0;38;5;${colors[4]}m\]\w"
 	PS1+="\[\e[0;38;5;${colors[5]}m\]\\\$"
-        PS1+="\[\e[0m\] "
-}
+	PS1+="\[\e[0m\] "
 
-main(){
-	if [[ -n "$1" ]]; then
-		ID="$1"
-	fi
-	if [[ "$ID" == "--auto" ]]; then
-		unset ID
-	fi
-	if [[ "$TERM" == "xterm" && "$SHELL" == "/bin/bash" ]] ; then
+else
+	PS1='\u@\h \w\$ '
+fi
 
-		if [[ -z "$ID" ]]; then
-			. /etc/os-release
-		fi
+unset os_family
 
-		case "$ID" in
-			ubuntu)		colors="202 241 230 090 127 255" ;;
-			void)		colors="036 255 114 255 113 255" ;;
-			*)		colors="227 221 215 209 203 197" ;;
-		esac
-		set_prompt "$colors"
-	else
-		PS1='\u@\h \w\$ '
-	fi
-}
-
-main "$@"
 
